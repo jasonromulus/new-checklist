a9f603b4e76e844b284536356fb90d84128f9c76
jasonromulus <pumcome@me.com>
2018-10-25 20:15:31 -0700

    Doing CRUD

--- checklist.py
+++ checklist.py
@@ -1,3 +1,21 @@
 print("Hello World")
 checklist = list()
 print (checklist)
+checklist.append('blue')
+print (checklist)
+checklist.append('ornage')
+print (checklist)
+
+def my_fun_function(say_this):
+    print(say_this)
+
+my_fun_function('Hello World')
+
+# create
+def create(item):
+    checklist.append('blue')
+
+# read
+def read(index):
+    item = checklist [index]
+    return item
