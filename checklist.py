fa91fa8706b9f3b23a27c96818d11701cf2ca6a9
jasonromulus <pumcome@me.com>
2018-10-25 20:38:26 -0700

    Finshed CRUD part of checklist

--- checklist.py
+++ checklist.py
@@ -1,24 +1,57 @@
-print("Hello World")
+# print("Hello World")
+# checklist = list()
+# print (checklist)
+# checklist.append('blue')
+# print (checklist)
+# checklist.append('ornage')
+# print (checklist)
+
+# def my_fun_function(say_this):
+#     print(say_this)
+
+# my_fun_function('Hello World')
+
+#Start of CRUD part of checklist project
 checklist = list()
-print (checklist)
-checklist.append('blue')
-print (checklist)
-checklist.append('ornage')
-print (checklist)
-
-def my_fun_function(say_this):
-    print(say_this)
-
-my_fun_function('Hello World')
 
 # create
 def create(item):
-    checklist.append('blue')
+    checklist.append (item)
 
 # read
 def read(index):
     return checklist [index]
 
-checklist = ['blue', 'orange']
-checklist [1] = 'cats'
-print (checklist)
+# checklist = ['blue', 'orange']
+# checklist [1] = 'cats'
+# print (checklist)
+
+# update
+def update (index, item):
+    checklist [index] = item
+
+# checklist = ['blue', 'cats']
+# checklist.pop (1)
+# print (checklist)
+
+def destroy (index):
+    checklist.pop (index)
+
+def test ():
+    create ("purple sox")
+    create ("red cloak")
+
+    print (read(0))
+    print (read(1))
+
+    update (0, "purple socks")
+    destroy (1)
+
+    print (read(0))
+    # print (read(1))
+
+test ()
+
+def list_all_items():
+    for list_item in checklist:
+        print (list_item)
