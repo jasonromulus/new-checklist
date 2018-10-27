746710916688216cc3b581ff0d7826781acce3c4
jasonromulus <pumcome@me.com>
2018-10-26 23:32:51 -0700

    Got it to work more!

--- checklist.py
+++ checklist.py
@@ -27,6 +27,7 @@ def read (index):
 # print (checklist)
 
 # update
+#
 def update (index, item):
     checklist [index] = item
 
@@ -46,7 +47,37 @@ def list_all_items ():
         index += 1
 
 #mark completed
-def mark_completed(index)
+def mark_completed(index):
+    update(index, "√" + checklist[int(index)])
+    #update (0, "purple socks")
+
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(int(item_index))
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
 
 # make test function
@@ -63,6 +94,25 @@ def test ():
     print (read(0))
     # print (read(1))
 
+    mark_completed (0)
     list_all_items ()
+
+    # Call your new function with the appropriate value
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+    # Continue until all code is run
+    select("P")
+    #View results
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
+    
+
 
 test ()
